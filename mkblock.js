41db37e4d997eca65e12692c34491a78045b4f67
weissfl <dmitry.pr@gmail.com>
2017-12-05 10:29:35 +0530

    При создании блока, блоку добавляеться кастомный класс

--- mkblock.js
+++ mkblock.js
@@ -56,7 +56,7 @@ if (blockName) {
 
         // Если это HTML
         else if (extention === 'html') {
-          fileContent = `<!--DEV\n\nДля использования этого файла как шаблона:\n\n@ @include('blocks/${blockName}/${blockName}.html')\n\n(Нужно убрать пробел между символами @)\nПодробнее: https://www.npmjs.com/package/gulp-file-include\n\n\n\n<div class="${blockName}">content</div>\n\n-->\n`;
+          fileContent = `<!--DEV\n\nДля использования этого файла как шаблона:\n\n@ @include('blocks/${blockName}/${blockName}.html', {"class": ""})\n\n(Нужно убрать пробел между символами @)\nПодробнее: https://www.npmjs.com/package/gulp-file-include\n\n\n\n<div class="${blockName} @@class">content</div>\n\n-->\n`;
           // fileCreateMsg = '';
         }
 
